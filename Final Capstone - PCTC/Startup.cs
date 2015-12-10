b51a217ce72bdde3d59b25c5b1acba59350c80fc
britcorona <BrittneyLCorona@gmail.com>
2015-12-10 10:22:40 -0600

    Adding solution file

---
+++ Final Capstone - PCTC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Final_Capstone___PCTC.Startup))]
+namespace Final_Capstone___PCTC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
