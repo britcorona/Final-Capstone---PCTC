d3a09545b7380516b9fea62d5e24331b8622636a
britcorona <BrittneyLCorona@gmail.com>
2015-12-10 14:02:59 -0600

    Created User an Content inside the TC tests

---
+++ Final Capstone - PCTC.Tests/Models/PCTCUserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Final_Capstone___PCTC.Models;
+
+namespace Final_Capstone___PCTC.Tests.Models
+{
+    [TestClass]
+    public class PCTCUserTests
+    {
+        [TestMethod]
+        public void PCTCUserEnsureICanCreateAnInstance()
+        {
+            PCTCUser a_user = new PCTCUser();
+            Assert.IsNotNull(a_user);
+        }
+
+        [TestMethod]
+        public void PCTCUserEnsureUserHasInformation()
+        {
+            PCTCUser a_user = new PCTCUser();
+
+            a_user.FirstName = "Brittney";
+            a_user.LastName = "Corona";
+
+            Assert.AreEqual("Brittney", a_user.FirstName);
+            Assert.AreEqual("Corona", a_user.LastName);
+        }
+
+
+    }
+}
---
+++ Final Capstone - PCTC/Models/PCTCUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final_Capstone___PCTC.Models
+{
+    public class PCTCUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
