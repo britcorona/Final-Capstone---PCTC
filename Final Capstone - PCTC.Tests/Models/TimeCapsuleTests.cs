2c389fb5602a6e303ae32f315cde3edf2e0394a5
britcorona <BrittneyLCorona@gmail.com>
2015-12-10 11:17:27 -0600

    Created TimeCapsule model and started basic testing

---
+++ Final Capstone - PCTC.Tests/Models/TimeCapsuleTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Final_Capstone___PCTC.Models;
+
+namespace Final_Capstone___PCTC.Tests.Models
+{
+    [TestClass]
+    public class TimeCapsuleTests
+    {
+        [TestMethod]
+        public void TCEnsureICanCreateAnInstance()
+        {
+            TimeCapsule a_tc = new TimeCapsule();
+            Assert.IsNotNull(a_tc);
+        }
+
+        [TestMethod]
+        public void TCEnsureCapsulesInformation()
+        {
+            TimeCapsule a_tc = new TimeCapsule();
+
+            DateTime expected_time = DateTime.Now;
+            a_tc.TimeCapsuleId = 1;
+            a_tc.ContentInsideTCId = 1;
+            a_tc.Name = "Name Here";
+            a_tc.Date = expected_time;
+
+            Assert.AreEqual(1, a_tc.TimeCapsuleId);
+            Assert.AreEqual(1, a_tc.ContentInsideTCId);
+            Assert.AreEqual("Name Here", a_tc.Name);
+            Assert.AreEqual(expected_time, a_tc.Date);
+        }
+    }
+}
---
+++ Final Capstone - PCTC/Models/TimeCapsule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Final_Capstone___PCTC.Models
+{
+    public class TimeCapsule
+    {
+        public int ContentInsideTCId { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        [Key]
+        public int TimeCapsuleId { get; set; }
+
+    }
+}
