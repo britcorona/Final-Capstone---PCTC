7ea33c7419505021b32ee1f33e5a2bce50238c2d
britcorona <BrittneyLCorona@gmail.com>
2015-12-14 10:21:58 -0600

    Refactoring Mock and setup Initialize an Cleanup tests

--- Final Capstone - PCTC.Tests/Models/PCTCRepositoryTests.cs
+++ Final Capstone - PCTC.Tests/Models/PCTCRepositoryTests.cs
@@ -11,17 +11,49 @@ namespace Final_Capstone___PCTC.Tests.Models
     [TestClass]
     public class PCTCRepositoryTests
     {
+        private Mock<PCTCContext> mock_context;
+        private Mock<DbSet<PCTCUser>> mock_set;
+        private PCTCRepository repo;
+
+        private void ConnectMocksToDataStore(IEnumerable<PCTCUser> data_store)
+        {
+            var data_source = data_store.AsQueryable<PCTCUser>();
+
+            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.Provider).Returns(data_source.Provider);
+            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.Expression).Returns(data_source.Expression);
+            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.ElementType).Returns(data_source.ElementType);
+            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.GetEnumerator()).Returns(data_source.GetEnumerator());
+
+            mock_context.Setup(a => a.PCTCUsers).Returns(mock_set.Object);
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<PCTCContext>();
+            mock_set = new Mock<DbSet<PCTCUser>>();
+            repo = new PCTCRepository(mock_context.Object);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            mock_context = null;
+            mock_set = null;
+            repo = null;
+        }
+
+
         [TestMethod]
         public void PCTCContextEnsureICanCreateInstance()
         {
-            PCTCContext context = new PCTCContext();
+            PCTCContext context = mock_context.Object;
             Assert.IsNotNull(context);
         }
 
         [TestMethod]
         public void PCTCRepositoryEnsureICanCreateInstance()
         {
-            PCTCRepository repo = new PCTCRepository();
             Assert.IsNotNull(repo);
         }
 
@@ -33,22 +65,20 @@ namespace Final_Capstone___PCTC.Tests.Models
                 new PCTCUser { FirstName = "Joe" },
                 new PCTCUser { FirstName = "Susie" }
             };
-            Mock<PCTCContext> mock_context = new Mock<PCTCContext>();
-            Mock<DbSet<PCTCUser>> mock_set = new Mock<DbSet<PCTCUser>>();
 
             mock_set.Object.AddRange(expected);
+            ConnectMocksToDataStore(expected);
 
-            var data_source = expected.AsQueryable();
-
-            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.Provider).Returns(data_source.Provider);
-            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.Expression).Returns(data_source.Expression);
-            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.ElementType).Returns(data_source.ElementType);
-            mock_set.As<IQueryable<PCTCUser>>().Setup(data => data.GetEnumerator()).Returns(data_source.GetEnumerator());
-            
-            mock_context.Setup(a => a.PCTCUsers).Returns(mock_set.Object);
-            PCTCRepository repo = new PCTCRepository(mock_context.Object);
             var actual = repo.GetAllUsers();
+            Assert.AreEqual("Joe", actual.First().FirstName);
             CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PCTCRepositoryEnsureIHaveAContext()
+        {
+            var actual = repo.Context;
+            Assert.IsInstanceOfType(actual, typeof(PCTCContext));
         }
     }
 }
