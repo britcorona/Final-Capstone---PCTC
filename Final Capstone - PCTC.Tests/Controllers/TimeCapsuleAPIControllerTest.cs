b0d086409c682669709b0e6e49c7f13bf1814e5f
britcorona <BrittneyLCorona@gmail.com>
2016-01-05 11:19:32 -0600

    Added button and form code

---
+++ Final Capstone - PCTC.Tests/Controllers/TimeCapsuleAPIControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Final_Capstone___PCTC.Controllers;
+
+namespace Final_Capstone___PCTC.Tests.Controllers
+{
+    [TestClass]
+    public class TimeCapsuleAPIControllerTest
+    {
+        [TestMethod]
+        public void TCAPIControllerEnsureICanCallGetAction()
+        {
+            TimeCapsuleAPIControllerTest my_controller = new TimeCapsuleAPIControllerTest();
+            string expected_output = "Is this working?";
+            string actual_output = my_controller.Get();
+            Assert.AreEqual(expected_output, actual_output);
+        }
+    }
+}
---
+++ Final Capstone - PCTC/Controllers/TimeCapsuleAPIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Final_Capstone___PCTC.Controllers
+{
+    public class TimeCapsuleAPIController : ApiController
+    {
+        public string Get()
+        {
+            return "Is this working?";
+        }
+        // GET: api/TimeCapsuleAPI
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/TimeCapsuleAPI/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TimeCapsuleAPI
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TimeCapsuleAPI/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TimeCapsuleAPI/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
